c4c849b76cc2d2a99312ef5cffb075f0e549156c
Apostolos Kalfas <kalfiniorandom@gmail.com>
2026-06-08 22:44:49 +0300

    Use TypeScript string fixture for Tailwind check test

--- src/Card.tsx
+++
@@ -1,7 +0,0 @@
-export function Card() {
-  return (
-    <div className="rounded-xl shadow-[0_0_10px_rgba(0,0,0,0.2)] p-4">
-      Hello
-    </div>
-  );
-}
---
+++ src/brokenTailwindClass.ts
@@ -0,0 +1,2 @@
+export const cardClass =
+  "rounded-xl shadow-[0_0_10px_rgba(0,0,0,0.2)] p-4";
